2e44daee457db74bf438b2b1b11d63c5dfd00a45
fena <79359861+fenasot@users.noreply.github.com>
2024-10-04 17:34:15 +0800

    update html and js, delete .keeps

--- js/13/functionExample.js
+++ js/13/functionExample.js
@@ -1,21 +1,40 @@
 /** 
- * aaaaa
- * 測試
- * aaa
- * 
+ * IIFE範例1(印出function本身)
+ * function expression
  */
-function TestA() {
- if (1 == 2) {
-
- }
+var helloWorld = function(user) {
+  console.log('helloWorld, ' + user);
 }
+
+/**
+ * IIFE範例2 (在建立時立即執行function)
+ */
+var sayWelcomeIIFEs = function(name) {
+  console.log('Welcome ' + name);
+}();
+
+/**
+ * IIFE範例3 (在建立時立即執行function，並附帶參數)
+ */
+ var sayWelcomeIIFEs = function(name) {
+  console.log('Welcome ' + name);
+}('PJCHEN');
+
+
 const numbers = [1, 2, 3, 4, 5];
         const evenNumber = (item) => item % 2 === 0;
         const newNumebers = numbers.filter(evenNumber);
         console.log(newNumebers); // [2, 4]
 
 
+/**
+ * 
+ */
+const test1 = () => {
+  // function expression
+  var helloWorld = function(name) {
+    console.log('Welcome, ' + name);
+  }
+  console.log(helloWorld);
+}
 
-addEventListener()
-.then(aaaaa)
-.catch()
